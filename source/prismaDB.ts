f329f046cbc4ca846f1fcde1b9f2e1b12e0bcb3b
Judah <judahholanda7@gmail.com>
2022-01-15 10:49:17 -0300

    update

--- source/prismaDB.ts
+++ source/prismaDB.ts
@@ -2,20 +2,20 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
-  PersistenceAdapter,
+  IPersistence,
   PersistenceInfo,
-  PersistencePromise,
+  IOutput,
   // RelationValueDAODB,
   // SelectedItemValue,
-  PersistenceInputCreate,
-  PersistenceInputUpdate,
-  PersistenceInputRead,
-  PersistenceInputDelete,
-  PersistenceInput,
+  IInputCreate,
+  IInputUpdate,
+  IInputRead,
+  IInputDelete,
+  IInput,
 } from 'flexiblepersistence';
 import { PrismaClient } from '@prisma/client';
 import { Default } from '@flexiblepersistence/default-initializer';
-export class PrismaDB implements PersistenceAdapter {
+export class PrismaDB implements IPersistence {
   element: { [name: string]: Default } = {};
   private persistenceInfo: PersistenceInfo;
   private prisma;
@@ -24,7 +24,7 @@ export class PrismaDB implements PersistenceAdapter {
     this.persistenceInfo = persistenceInfo;
     this.prisma = new PrismaClient();
   }
-  other(input: PersistenceInput<any>): Promise<PersistencePromise<any>> {
+  other(input: IInput<any>): Promise<IOutput<any, any>> {
     throw new Error('Method not implemented.');
   }
 
@@ -83,7 +83,7 @@ export class PrismaDB implements PersistenceAdapter {
       .then((output) => {
         console.log(output);
 
-        const persistencePromise: PersistencePromise<any> = {
+        const persistencePromise: IOutput<any, any> = {
           receivedItem: output,
           result: output,
           selectedItem: input.selectedItem,
@@ -97,15 +97,13 @@ export class PrismaDB implements PersistenceAdapter {
       });
   }
 
-  private makePromise(input, method): Promise<PersistencePromise<any>> {
+  private makePromise(input, method): Promise<IOutput<any, any>> {
     return new Promise((resolve, reject) => {
       this.persistencePromise(input, method, resolve, reject);
     });
   }
 
-  correct(
-    input: PersistenceInputUpdate<any>
-  ): Promise<PersistencePromise<any>> {
+  correct(input: IInputUpdate<any>): Promise<IOutput<any, any>> {
     //! Envia o input para o service determinado pelo esquema e lá ele faz as
     //! operações necessárias usando o journaly para acessar outros DAOs ou
     //! DAOs.
@@ -116,34 +114,32 @@ export class PrismaDB implements PersistenceAdapter {
     return this.update(input);
   }
 
-  nonexistent(input: PersistenceInputDelete): Promise<PersistencePromise<any>> {
+  nonexistent(input: IInputDelete): Promise<IOutput<any, any>> {
     return this.delete(input);
   }
 
-  existent(
-    input: PersistenceInputCreate<any>
-  ): Promise<PersistencePromise<any>> {
+  existent(input: IInputCreate<any>): Promise<IOutput<any, any>> {
     return this.create(input);
   }
 
-  create(input: PersistenceInputCreate<any>): Promise<PersistencePromise<any>> {
+  create(input: IInputCreate<any>): Promise<IOutput<any, any>> {
     // console.log('CREATE:', input);
     return Array.isArray(input.item)
       ? this.makePromise(input, 'createMany')
       : this.makePromise(input, 'create');
   }
-  update(input: PersistenceInputUpdate<any>): Promise<PersistencePromise<any>> {
+  update(input: IInputUpdate<any>): Promise<IOutput<any, any>> {
     return input.single
       ? this.makePromise(input, 'updateFirst')
       : this.makePromise(input, 'updateMany');
   }
-  read(input: PersistenceInputRead): Promise<PersistencePromise<any>> {
+  read(input: IInputRead): Promise<IOutput<any, any>> {
     // console.log('read', input);
     return input.single
       ? this.makePromise(input, 'findFirst')
       : this.makePromise(input, 'findMany');
   }
-  delete(input: PersistenceInputDelete): Promise<PersistencePromise<any>> {
+  delete(input: IInputDelete): Promise<IOutput<any, any>> {
     // console.log('FUCKING DELETE');
 
     return input.single
